c9f52e3b6a14cf63d9ed5eafbc7629f22c6f2382
Chinyenwu <35883811+Chinyenwu@users.noreply.github.com>
2018-08-05 17:45:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname ));
-
+var roomno = 0;
 function onConnection(socket){
+  roomno=roomno+1;
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+    socket.on('pressed', function(key){
+        if(key === 38){
+            socket.emit('PlayersMoving', key);
+            socket.broadcast.emit('PlayersMoving', key);
+        } 
+    });
+	console.log(roomno);
 }
-
 io.on('connection', onConnection);
 
 http.listen(port, () => console.log('listening on port ' + port));
